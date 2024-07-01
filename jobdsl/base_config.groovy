e4ee54a5d6ef015edc84bf26fd108c0d2d0497a8
mawhaze <evancast92@gmail.com>
2024-07-01 16:08:04 -0600

    adding base dsl and image config

---
+++ jobdsl/base_config.groovy
@@ -0,0 +1,54 @@
+// set up base dsl for job for jenkins
+job('root-dsl-seed') {
+  description('Seed job for Jenkins Job DSL')
+  logRotator {
+    numToKeep(10)
+  }
+  scm {
+    git {
+      remote {
+        url('https://github.com/Mawhaze/jenkins.git')
+        credentials('github_access_token')
+      }
+      triggers{
+        scm('H/15 * * * *')
+      }
+      steps {
+        dsl {
+          external('jenkins/jobdsl/*.groovy')
+        }
+      }
+
+// set up root folder strupcture and seed jobs
+folder('ansible') {
+  description('Ansible jobs for Jenkins')
+}
+
+job('ansible/ansible-dsl-seed') {
+  description('Seed job for Jenkins Job DSL')
+  logRotator {
+    numToKeep(10)
+  }
+  scm {
+    git {
+      remote {
+        url('https://github.com/Mawhaze/ansible.git')
+        credentials('github_access_token')
+      }
+      triggers{
+        scm('H/15 * * * *')
+      }
+      steps {
+        dsl {
+          external('ansible/jenkins/jobdsl/*.groovy')
+        }
+      }
+
+// set up docker folder structure 
+// seed jobs should be configured in their respective repositories
+folder('docker') {
+  description('Docker jobs for Jenkins')
+
+folder('docker/build') {
+  description('Docker build jobs for Jenkins')
+}
