d40e88610ba0e15b3d14a0e9712647f593855018
mawhaze <evancast92@gmail.com>
2024-07-15 17:03:48 -0600

    adding packer folder and dsl seed

--- jobdsl/base_config.groovy
+++ jobdsl/base_config.groovy
@@ -110,6 +110,34 @@ pipelineJob('docker/build/jenkins_agent') {
   }
 }
 
+// set up packer folder structure
+folder('packer') {
+  description('Packer jobs for Jenkins')
+}
+
+job('packer/packer-dsl-seed') {
+  description('Seed job for Jenkins Job DSL')
+  logRotator {
+    numToKeep(10)
+  }
+  scm {
+    git {
+      remote {
+        url('https://github.com/Mawhaze/packer.git')
+        credentials('github_access_token')
+      }
+      triggers{
+        scm('H/15 * * * *')
+      }
+      steps {
+        dsl {
+          external('jenkins/jobdsl/*.groovy')
+        }
+      }
+    }
+  }
+}
+
 // set up project-ender folder structure
 folder('project-ender') {
   description('Project Ender jobs for Jenkins')
